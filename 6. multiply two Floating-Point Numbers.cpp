eee31aabc8cfa3af962dc9fd713bd26ad90e6e5b
MD Nayem Hossain <mdnayemhossian128@gmail.com>
2026-03-19 19:49:21 +0600

    Add files via upload

---
+++ 6. multiply two Floating-Point Numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double n1, n2, product;
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    product = n1 * n2;
+    cout << "Product = " << product << endl;
+    return 0;
+}
